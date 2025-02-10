968efd9a72049718af9dcf39d59a4e69e77a6d64
jaeyeong <young042510@gmail.com>
2025-02-11 04:37:52 +0900

    settings in PenshopApplication

---
+++ src/main/java/com/reminder/penshop/PenshopApplication.java
@@ -0,0 +1,13 @@
+package com.reminder.penshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenshopApplication.class, args);
+	}
+
+}
