f726d36c0237c3817a662ef9f9f226d1a468e680
jaeyeong <young042510@gmail.com>
2025-02-13 15:00:14 +0900

    feat PointDTO 추가

---
+++ src/main/java/com/reminder/penshop/cart/model/dto/PointDTO.java
@@ -0,0 +1,16 @@
+package com.reminder.penshop.cart.model.dto;
+
+
+import lombok.Data;
+
+@Data
+public class PointDTO {
+
+    private int pointNo;
+    private String paymentNo;
+    private String bonusReason;
+    private int pointAmount;
+    private String pointDateTime;
+    private String pointStatus;
+    private PaymentDTO payment;
+}
