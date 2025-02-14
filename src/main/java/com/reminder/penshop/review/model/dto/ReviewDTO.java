c43aeef90db43ae067b1d3dd1d87c3dc801d4e10
jaeyeong <young042510@gmail.com>
2025-02-14 23:17:15 +0900

    feat 리뷰관련 기능 추가

--- src/main/java/com/reminder/penshop/review/model/dto/ReviewDTO.java
+++ src/main/java/com/reminder/penshop/review/model/dto/ReviewDTO.java
@@ -1,11 +1,16 @@
 package com.reminder.penshop.review.model.dto;
 
+import com.reminder.penshop.cart.model.dto.DeliveryDTO;
+import com.reminder.penshop.cart.model.dto.OrderDTO;
+import com.reminder.penshop.cart.model.dto.OrderDetailDTO;
+import com.reminder.penshop.member.model.dto.MemberDTO;
 import com.reminder.penshop.product.model.dto.OptionDTO;
 import com.reminder.penshop.product.model.dto.ProductDTO;
 import com.reminder.penshop.upload.model.dto.AttachmentDTO;
 import lombok.Data;
 
-import java.util.Date;
+
+import java.sql.Date;
 import java.util.List;
 
 @Data
@@ -22,9 +27,9 @@ public class ReviewDTO {
     private int revwRatings;
     private OptionDTO option;
     private ProductDTO product;
-    // private OrderDTO order;
-    // private OrderDetailDTO orderDetail;
-    // private DeliveryDTO delivery;
-    // private MemberDTO member;
+    private OrderDTO order;
+    private OrderDetailDTO orderDetail;
+    private DeliveryDTO delivery;
+    private MemberDTO member;
     private List<AttachmentDTO> attachmentList;
 }
