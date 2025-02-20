06952c382e080743f1ff832b51d8300c3b1bd473
jaeyeong <young042510@gmail.com>
2025-02-20 14:09:27 +0900

    feat 회원가입, 로그인, 로그아웃, 아이디, 비밀번호 찾기 추가

--- src/main/java/com/reminder/penshop/member/controller/MemberController.java
+++ src/main/java/com/reminder/penshop/member/controller/MemberController.java
@@ -6,8 +6,8 @@ import com.reminder.penshop.member.model.dto.UserImpl;
 import com.reminder.penshop.member.model.service.MemberService;
 import com.reminder.penshop.oauth.model.service.KakaoOAuth2Service;
 import com.reminder.penshop.validator.SignUpValidator;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,35 +34,33 @@ import java.util.UUID;
 public class MemberController {
 
     private final MemberService memberService;
+    private final KakaoOAuth2Service kakaoOAuth2Service;
     private final MailService mailService;
     private final MessageSource messageSource;
     private final PasswordEncoder passwordEncoder;
     private final SignUpValidator signUpValidator;
-    private final KakaoOAuth2Service kakaoOAuth2Service;
 
     @Autowired
-    public MemberController(MemberService memberService, MailService mailService, MessageSource messageSource, PasswordEncoder passwordEncoder, SignUpValidator signUpValidator, KakaoOAuth2Service kakaoOAuth2Service) {
+    public MemberController(MemberService memberService, KakaoOAuth2Service kakaoOAuth2Service, MailService mailService, MessageSource messageSource, PasswordEncoder passwordEncoder, SignUpValidator signUpValidator) {
         this.memberService = memberService;
+        this.kakaoOAuth2Service = kakaoOAuth2Service;
         this.mailService = mailService;
         this.messageSource = messageSource;
         this.passwordEncoder = passwordEncoder;
         this.signUpValidator = signUpValidator;
-        this.kakaoOAuth2Service = kakaoOAuth2Service;
     }
 
-    /*
-     * MessageSource
+    /* MessageSource
      * 1. ContextConfiguration 통해 Bean 등록
      * 2. classpath 하위에 messages 폴더 및 properties 파일 생성
-     * */
+     */
 
-    /*
-     *  아이디 중복 검사
-     *  @return 중복된 아이디 개수
-     * */
-    @PostMapping(value = "/checkId", produces = "application/json; charset=UTF-8")
-    @ResponseBody
-    //ajax 통신에서 JSON 응답을 치르기 위해서는 어노테이션 추가, spring-web의 경우 jackson-databind 등 기본적으로 추가돼 있는 의존성들을 고려하여야 함
+    /**
+     * 아이디 중복 검사
+     * @return 중복된 아이디 개수
+     */
+    @PostMapping(value="/checkId", produces="application/json; charset=UTF-8")
+    @ResponseBody	//ajax 통신에서 JSON 응답을 치르기 위해서는 어노테이션 추가, spring-web의 경우 jackson-databind 등 기본적으로 추가돼 있는 의존성들을 고려하여야 함
     public int checkId(String memberId) {
 
         log.info("checkId 시작");
@@ -73,40 +71,48 @@ public class MemberController {
         return result;
     }
 
-    /*
-     *  이메일 중복검사
-     *  @return 중복된 이메일 개수
-     * */
-    @PostMapping(value = "/checkEmail", produces = "application/json; charset=UTF-8")
+    /**
+     * 이메일 중복 검사
+     * @return 중복된 이메일 개수
+     */
+    @PostMapping(value="/checkEmail", produces="application/json; charset=UTF-8")
     @ResponseBody
     public int checkEmail(String email) {
 
-        log.info("이메일 중복 검사");
-        log.info("Check email : {}", email);
+        log.info("checkEmail 시작");
+        log.info("전달 받은 Email : {}", email);
         int result = memberService.checkEmail(email);
-        log.info("Check email 결과 : {}", result);
+        log.info("checkEmail 결과 : {}", result);
 
         return result;
     }
 
     /**
      * 회원가입
-     *
      * @return 회원가입 폼 또는 에러 페이지로 연결
      */
     @GetMapping("signup")
-    public String signupForm(@AuthenticationPrincipal UserImpl user, Model model) {
-        if (user != null) { // 이미 로그인된 회원 재요청하는 경우 denied 페이지로 연결
-            model.addAttribute("errorMessage", "이미 로그인한 상태이거나 접근이 불가능합니다.");
-            return "common/denied";
+    public String signUpForm(@AuthenticationPrincipal UserImpl user, Model model) {
+        if(user != null) { //이미 로그인된 회원이 임의로 재요청하는 경우 denied 페이지로 연결
+            model.addAttribute("errorMessage", "이미 로그인한 상태이거나 접근 권한이 없는 페이지입니다.");
+            return "/common/denied";
         }
-        return "member/signup";
+        return "/member/signup";
     }
 
     @PostMapping("signup")
-    public Object signupMember(@Validated @ModelAttribute("member") MemberDTO member, BindingResult bindingResult
-            , @RequestParam Map<String, String> params, RedirectAttributes rttr, Model model, Locale locale) throws Exception {
-        /* 회원가입 성공로직 */
+    public Object signUpMember(@Validated @ModelAttribute("member") MemberDTO member, BindingResult bindingResult,
+                               @RequestParam Map<String, String> params, RedirectAttributes rttr, Model model, Locale locale) throws Exception { //BindingResult는 유효성 검사 대상 객체 바로 뒤에 선언
+		/*
+		signUpValidator.validate(member, bindingResult);
+
+		if(bindingResult.hasErrors()) { //검증 오류 발생 시 입력 폼으로 리턴
+			log.info("검증 오류 발생 : {}", bindingResult);
+			return "signup";
+		}
+		*/
+
+        /* 회원가입 성공 로직 */
         String phone = params.get("phoneA") + params.get("phoneB") + params.get("phoneC");
         String address = params.get("postalCode") + "$" + params.get("address") + "$" + params.get("detailAddress");
         member.setPhone(phone);
@@ -127,23 +133,21 @@ public class MemberController {
 
     /**
      * 회원가입 후 이메일 인증 및 계정 활성화
-     *
      * @throws NotFoundException
      */
     @GetMapping("verify")
-    public String signupEmailVerification(@ModelAttribute("token") String token, RedirectAttributes rttr
-            , Model model, Locale locale) throws NotFoundException {
+    public String signUpEmailVerification(@ModelAttribute("token") String token, RedirectAttributes rttr, Locale locale) throws NotFoundException {
         String email = mailService.verificationEmail(token);
-        log.info("인증 emil : {}", email);
+        log.info("인증 email : {}", email);
 
-        if (email == null || token == null) { // 이미 인증 끝난 계정 or 만료된 토큰으로 재요청
+        if(email == null || token == null) { //이미 인증 마친 계정 또는 만료된 토큰으로의 재요청
             log.info("올바르지 않은 접근");
             rttr.addFlashAttribute("verificationMessage", messageSource.getMessage("accessNotAllowed", null, locale));
             return "redirect:/";
         }
 
         boolean isRegistered = memberService.checkEmail(email) > 0 ? true : false;
-        if (!isRegistered) {
+        if(!isRegistered) {
             log.info("인증 실패");
             rttr.addFlashAttribute("verificationMessage", messageSource.getMessage("emailVerificationFailed", null, locale));
             return "redirect:/";
@@ -157,7 +161,6 @@ public class MemberController {
 
     /**
      * 로그인
-     *
      * @return 로그인 폼 또는 에러 페이지로 연결
      */
     @GetMapping("signin")
@@ -165,8 +168,7 @@ public class MemberController {
         /* 소셜로그인 URL */
         String kakaoUrl = kakaoOAuth2Service.getKakaoSignInUrl();
         model.addAttribute("kakaoUrl", kakaoUrl);
-
-        if (user != null) { //이미 로그인된 회원이 임의로 재요청하는 경우 denied 페이지로 연결
+        if(user != null) { //이미 로그인된 회원이 임의로 재요청하는 경우 denied 페이지로 연결
             model.addAttribute("loginAccessDenied", messageSource.getMessage("loginAccessDenied", null, locale));
             return "/common/denied";
         }
@@ -175,42 +177,43 @@ public class MemberController {
     }
 
     @PostMapping("signin")
-    public String signInMember(@RequestParam(required = false) String errorTitle, @RequestParam(required = false) String errorText,
-                               @RequestParam(required = false) String resetPasswordRequired, HttpServletRequest request, HttpSession session,
+    public String signInMember(@RequestParam(required=false) String errorTitle, @RequestParam(required=false) String errorText,
+                               @RequestParam(required=false) String resetPasswordRequired, HttpServletRequest request, HttpSession session,
                                RedirectAttributes rttr, Locale locale) {
         /*
          * [Handler 이동 전 요청별 확인]
-         * 누구? 바로그인  상태 회원
-         * 무엇을? 1) 상세페이지 바로주문 요청 -  @SessionAttributes 어노테이션 활용해 선택값을 session상에 임시 저장
-         *       2) 이전 경로가 존재 -> 이름 sessison상에 임시 저장
-         * */
+         * who : 비로그인 상태의 회원
+         * what : 1)상품상세페이지 > '바로주문' 요청 => @SessionAttributes 어노테이션 활용해 선택값을 session상에 임시 저장
+         * 		  2)이전 경로가 존재 => 이를 session상에 임시 저장
+         */
         String[] detailOptionNo = request.getParameterValues("orderOptionNo");
         String[] detailOptionQt = request.getParameterValues("orderOptionQt");
         session.setAttribute("detailOptionNo", detailOptionNo);
         session.setAttribute("detailOptionQt", detailOptionQt);
 
-        String uri = request.getHeader("Referer"); // 로그인 직전 페이지 불러오기
+        String uri = request.getHeader("Referer"); //사용자의 이전 경로
 
         String loginMember = (String) session.getAttribute("loginMember");
-        if (loginMember == null) {
-            if (uri != null && !(uri.contains("/signin"))) { //돌아가야 할 이전 경로가 존재하고, 사용자가 직접 로그인페이지를 요청한 것이 아닌 경우
+        if(loginMember == null) {
+            if(uri != null && !(uri.contains("/signin"))) { //돌아가야 할 이전 경로가 존재하고, 사용자가 직접 로그인페이지를 요청한 것이 아닌 경우
                 request.getSession().setAttribute("prevPage", uri); //이전 경로를 session상에 저장하여 LoginSuccessHandler 통해 처리
             }
         }
 
         /*
-         *  임시 비밀번호로 로그인 성공
-         *  : 바로 회원정보수정 페이지로 이동
-         * */
-        if (resetPasswordRequired != null) {
+         * 임시 비밀번호로 로그인에 성공
+         * : '회원정보수정' 페이지로 이동
+         */
+        if(resetPasswordRequired != null ) {
             rttr.addFlashAttribute("resetPasswordRequired", messageSource.getMessage("resetPasswordRequired", null, locale));
             return "redirect:/mypage/change";
         }
+
         /*
          * 로그인 실패
          * : LoginFailureHandler로부터 @RequestParam 어노테이션 통해 넘어온 에러메시지 출력
          */
-        if (errorTitle != null) {
+        if(errorTitle != null) {
             rttr.addFlashAttribute("signInMessageTitle", errorTitle);
             rttr.addFlashAttribute("signInMessageText", errorText);
             return "redirect:/member/signin";
@@ -218,13 +221,14 @@ public class MemberController {
         return uri;
     }
 
-    /*
-    *  로그아웃
-    * */
+    /**
+     * 로그아웃
+     */
     @GetMapping("signout")
     public void signOut(SessionStatus status) {
         status.setComplete(); //@SessionAttributes 어노테이션과 함께 session에 저장했던 속성 삭제
     }
+
 
     /**
      * 휴대폰 본인인증 결과 반영
