8ef6d6d9f024fc5d339f26882f10e823d3ec3119
jaeyeong <young042510@gmail.com>
2025-02-24 20:23:45 +0900

    feat: 카테고리 정보 및 섹션 분류를 위한 DTO 클래스 추가

---
+++ src/main/java/com/reminder/penshop/product/model/dto/CategoryDTO.java
@@ -0,0 +1,11 @@
+package com.reminder.penshop.product.model.dto;
+
+import lombok.Data;
+
+@Data
+public class CategoryDTO {
+	
+	private int categoryNo;
+	private String categoryName;
+	private String categorySection;
+}
