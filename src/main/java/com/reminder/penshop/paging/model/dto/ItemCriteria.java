d2862a04f401c654ef9186bb680270931253c3af
jaeyeong <young042510@gmail.com>
2025-02-24 16:55:40 +0900

    feat: Criteria와 ItemCriteria 클래스를 분리하여 게시판과 상품 목록의 다른 요구사항 대응

---
+++ src/main/java/com/reminder/penshop/paging/model/dto/ItemCriteria.java
@@ -0,0 +1,44 @@
+package com.reminder.penshop.paging.model.dto;
+
+import org.springframework.web.util.UriComponentsBuilder;
+
+import lombok.Data;
+
+@Data
+public class ItemCriteria {
+
+	private int page;
+	private int items; //per page
+	private String section;
+	private String category;
+	private String brand;
+	private int min; //min price
+	private int max; //max price
+	private String tag;
+	private String color; //ink color
+	private String sortBy;
+	
+	public ItemCriteria() {
+		this(1, 8);
+	}
+	
+	public ItemCriteria(int page, int items) {
+		this.page = page;
+		this.items = items;
+	}
+	
+	public String getListLink() {
+		UriComponentsBuilder builder = UriComponentsBuilder.fromPath("")
+				.queryParam("page", page)
+				.queryParam("items", items)
+				.queryParam("section", section)
+				.queryParam("category", this.category)
+				.queryParam("brand", this.brand)
+				.queryParam("min", this.min)
+				.queryParam("max", this.max)
+				.queryParam("tag", this.tag)
+				.queryParam("color", this.color)
+				.queryParam("sortBy", this.sortBy);
+		return builder.toUriString();
+	}
+}
