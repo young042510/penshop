ecd5b9647fcafd380604cc931e61dec3ad717bfe
jaeyeong <young042510@gmail.com>
2025-02-24 21:25:20 +0900

    feat: 휴지통 데이터 전송 객체

---
+++ src/main/java/com/reminder/penshop/admin/model/dto/TrashDTO.java
@@ -0,0 +1,18 @@
+package com.reminder.penshop.admin.model.dto;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class TrashDTO {
+
+	private int trashNo;
+	private String refBoard;
+	private int refPostNo;
+	private String trashTitle;
+	private String trashWriter;
+	private String trashDeleteBy;
+	private Date trashDateMoved;
+	private Date trashDateUntil;
+}
