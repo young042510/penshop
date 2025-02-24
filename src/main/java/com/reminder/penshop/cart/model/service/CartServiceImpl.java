b1c2a3a4e9e63904de7c729de7886a170d89f1f5
jaeyeong <young042510@gmail.com>
2025-02-24 21:14:44 +0900

    feat: 장바구니 서비스 구현

---
+++ src/main/java/com/reminder/penshop/cart/model/service/CartServiceImpl.java
@@ -0,0 +1,53 @@
+package com.reminder.penshop.cart.model.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.reminder.penshop.cart.model.dao.CartMapper;
+import com.reminder.penshop.cart.model.dto.CartDTO;
+import com.reminder.penshop.product.model.dto.BrandDTO;
+import com.reminder.penshop.product.model.dto.OptionDTO;
+import com.reminder.penshop.product.model.dto.ProductDTO;
+
+@Service("cartService")
+public class CartServiceImpl implements CartService {
+
+	private CartMapper cartMapper;
+	
+	@Autowired
+	public CartServiceImpl(CartMapper cartMapper) {
+		this.cartMapper = cartMapper;
+	}
+	
+	@Override
+	public List<CartDTO> getCurrentCart(String memberId) {
+		return cartMapper.getCurrentCart(memberId);
+	}
+	
+	@Override
+	public CartDTO getCartItemByOptionNo(String username, int optionNo) {
+		return cartMapper.getCartItemByOptionNo(username, optionNo);
+	}
+
+	@Override
+	public List<CartDTO> getCartItemList(CartDTO cartDTO) {
+		return cartMapper.getCartItemList(cartDTO);
+	}
+
+	@Override
+	public OptionDTO searchOptionInfoByOptionNo(int optionNo) {
+		return cartMapper.searchOptionInfoByOptionNo(optionNo);
+	}
+
+	@Override
+	public ProductDTO searchProductInfoByOptionNo(int optionNo) {
+		return cartMapper.searchProductInfoByOptionNo(optionNo);
+	}
+
+	@Override
+	public BrandDTO searchBrandInfoByOptionNo(int optionNo) {
+		return cartMapper.searchBrandInfoByOptionNo(optionNo);
+	}
+}
