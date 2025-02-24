0f39839a436991c37cdf7b3143c4d52f44e5a304
jaeyeong <young042510@gmail.com>
2025-02-24 16:42:20 +0900

    feat: 댓글 관련 DB 매핑 인터페이스

---
+++ src/main/java/com/reminder/penshop/community/model/dao/CommentMapper.java
@@ -0,0 +1,28 @@
+package com.reminder.penshop.community.model.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.reminder.penshop.community.model.dto.CommentDTO;
+
+@Mapper
+public interface CommentMapper {
+
+	List<CommentDTO> getCommentsInPost(String refBoard, int refPostNo);
+	
+	List<CommentDTO> getNestedCommentsInPost(String refBoard, int refPostNo);
+
+	int writeAComment(CommentDTO commentDTO);
+	
+	int editAComment(int commentNo, String commentContent);
+
+	int deleteAComment(int commentNo);
+
+	int getTotalCommentNumber(String refBoard, int refPostNo);
+
+	Map<String, String> checkInquiryPostWriter(int refPostNo);
+
+	CommentDTO checkParentComment(int commentNestedTo);
+}
