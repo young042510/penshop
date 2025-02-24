a57bd0f74aa44400ffeb55d75f4a8cdb4a442427
jaeyeong <young042510@gmail.com>
2025-02-24 21:28:47 +0900

    feat: 메인 애플리케이션

---
+++ src/main/java/com/reminder/penshop/config/PenshopApplication.java
@@ -0,0 +1,14 @@
+package com.reminder.penshop.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenshopApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PenshopApplication.class, args);
+	}
+
+}
