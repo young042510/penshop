ba2504dde4e10c77f71417d01a5545b3b69f7bc7
jaeyeong <young042510@gmail.com>
2025-02-24 20:25:18 +0900

    feat: 상품 옵션 관리를 위한 DTO 클래스 추가

---
+++ src/main/java/com/reminder/penshop/product/model/dto/OptionDTO.java
@@ -0,0 +1,21 @@
+package com.reminder.penshop.product.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OptionDTO {
+
+	private int optionNo;
+	private int refProdNo;
+	private int refStockNo;
+	private String bodyColor;
+	private String inkColor;
+	private double pointSize;
+	private int extraCharge;
+	private StockDTO stock;
+	private ProductDTO product;
+}
