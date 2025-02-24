04f07c8a19f2e1cb9b5e7c086e0edcaee36a2353
jaeyeong <young042510@gmail.com>
2025-02-24 16:54:23 +0900

    feat: 페이징 처리를 위한 Criteria 구현

---
+++ src/main/java/com/reminder/penshop/paging/model/dto/Criteria.java
@@ -0,0 +1,32 @@
+package com.reminder.penshop.paging.model.dto;
+
+import org.springframework.web.util.UriComponentsBuilder;
+
+import lombok.Data;
+
+@Data
+public class Criteria {
+	
+	private int currentPageNo;
+	private int recordsPerPage;
+	private String condition;
+	private String keyword;
+	
+	public Criteria() {
+		this(1, 10);
+	}
+	
+	public Criteria(int currentPageNo, int recordsPerPage) {
+		this.currentPageNo = currentPageNo;
+		this.recordsPerPage = recordsPerPage;
+	}
+	
+	public String getListLink() {
+		UriComponentsBuilder builder = UriComponentsBuilder.fromPath("")
+				.queryParam("currentPageNo", currentPageNo)
+				.queryParam("recordsPerPage", recordsPerPage)
+				.queryParam("category", this.getCondition())
+				.queryParam("keyword", this.keyword);
+		return builder.toUriString();
+	}
+}
