1bf4231ae34041be2c246927a24ae0ea4ef77e0b
jaeyeong <young042510@gmail.com>
2025-02-24 21:26:01 +0900

    feat: SSE 컨트롤러 구현

---
+++ src/main/java/com/reminder/penshop/notification/controller/SseController.java
@@ -0,0 +1,32 @@
+package com.reminder.penshop.notification.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import com.reminder.penshop.member.model.dto.UserImpl;
+import com.reminder.penshop.notification.model.service.SseService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+public class SseController {
+
+	private final SseService sseService;
+
+	/**
+	 * 로그인 시 SSE 구독(연결)
+	 */
+	@GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public ResponseEntity<SseEmitter> subscribe(@AuthenticationPrincipal UserImpl user,
+			@RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "") String lastEventId) {
+		return ResponseEntity.ok(sseService.subscribe(user.getMemberId(), lastEventId));
+	}
+}
