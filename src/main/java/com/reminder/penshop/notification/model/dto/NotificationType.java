b601468aab15a269860fb48429aa99c9b94957ad
jaeyeong <young042510@gmail.com>
2025-02-24 21:22:38 +0900

    feat: 알림 유형 열거형

---
+++ src/main/java/com/reminder/penshop/notification/model/dto/NotificationType.java
@@ -0,0 +1,26 @@
+package com.reminder.penshop.notification.model.dto;
+
+public enum NotificationType {
+	
+	COMMENT("/cs/inquiry/details?no=", "comment"),
+	REPLY("/cs/inquiry/details?no=", "nestedComment"),
+	DISPATCH("/mypage/order/details?no=", "dispatch"),
+	DELIVERY("/mypage/review", "delivery"),
+	NOTICE("/cs/main", "notice");
+	
+	private final String path;
+	private final String alias;
+	
+	NotificationType(String path, String alias) {
+		this.path = path;
+		this.alias = alias;
+	}
+	
+	public String getPath() {
+		return path;
+	}
+	
+	public String getAlias() {
+		return alias;
+	}
+}
